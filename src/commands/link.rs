d68b506324b103d4cb9b6e532f951ffb157e3508
StygianLightning <denrellum@gmail.com>
2021-03-21 18:52:12 +0100

    Retrieve channels manually via the passed-in names.
    We cannot use msg.channel_mentions because that does not work for private channels as per Discord documentation.

--- src/commands/link.rs
+++ src/commands/link.rs
@@ -1,10 +1,24 @@
 use serenity::client::Context;
 use serenity::framework::standard::{macros::command, CommandResult};
-use serenity::model::channel::Message;
+use serenity::model::channel::{Embed, GuildChannel, Message};
+use serenity::model::id::ChannelId;
+use serenity::utils::MessageBuilder;
+use std::collections::HashMap;
+use std::str::FromStr;
 
 #[command]
 #[description = "Link this channel to another; messages from this channel will be re-posted to the linked channel"]
 pub async fn link(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "lanked").await?;
+    for channel in msg.content.split_ascii_whitespace().skip(1) {
+        let guild: HashMap<ChannelId, GuildChannel> = msg.guild_id.unwrap().channels(ctx).await?;
+        let channel = guild.get(&ChannelId::from_str(channel)?);
+
+        if let Some(channel) = channel {
+            println!("found channel {:?}", channel);
+        }
+    }
+
+    msg.reply(ctx, "Lanked").await?;
+
     Ok(())
 }
