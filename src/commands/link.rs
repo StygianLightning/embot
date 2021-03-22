ceb5548b55c0ca01100cc5011813f83f36cec8b5
StygianLightning <denrellum@gmail.com>
2021-03-22 22:27:56 +0100

    Restricted link command to admins.

--- src/commands/link.rs
+++ src/commands/link.rs
@@ -9,6 +9,7 @@ use std::str::FromStr;
 
 #[command]
 #[only_in(guilds)]
+#[required_permissions("ADMINISTRATOR")]
 #[description = "Link this channel to another; messages from this channel will be re-posted to the linked channel"]
 pub async fn link(ctx: &Context, msg: &Message) -> CommandResult {
     for channel in msg.content.split_ascii_whitespace().skip(1) {
